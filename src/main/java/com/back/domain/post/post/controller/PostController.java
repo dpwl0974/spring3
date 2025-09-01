4e0e587683e1106e590178810814132116823deb
dpwl0974 <dpwl0974@gmail.com>
2025-09-01 15:20:03 +0900

    feat: 글 작성 처리 & 보이기 기능 분리 구현

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -1,5 +1,7 @@
 package com.back.domain.post.post.controller;
 
+import com.back.domain.post.post.entity.Post;
+import com.back.domain.post.post.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,12 +9,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class PostController {
 
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping("/posts/write")
     @ResponseBody
     public String write() {
 
         return """
-                <form action="https://localhost:8080/">
+                <form action="/posts/doWrite">
                   <input type="text" name="title">
                   <br>
                   <textarea name="content"></textarea>
@@ -20,5 +28,17 @@ public class PostController {
                   <input type="submit" value="작성">
                 </form>
                 """;
+    }
+
+    @GetMapping("/posts/doWrite")
+    @ResponseBody
+    public String doWrite(
+            String title,
+            String content
+    ) {
+
+        Post post = postService.write(title, content);
+
+        return "%d번 글이 작성되었습니다.".formatted(post.getId());
     }
 }
