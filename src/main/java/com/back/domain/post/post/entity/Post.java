6af6e6966209f22472339cf019b8b21164ee54c6
dpwl0974 <dpwl0974@gmail.com>
2025-09-01 14:28:39 +0900

    feat: 샘플 데이터 3개 생성

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,13 +1,20 @@
 package com.back.domain.post.post.entity;
 
 import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
 @NoArgsConstructor
 @Getter
+@Entity
 public class Post extends BaseEntity {
     private String title;
     private String content;
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
