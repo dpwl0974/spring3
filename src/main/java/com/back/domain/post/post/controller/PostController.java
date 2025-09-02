e76822226ac68a280cb69c14e9bba7f126652d5b
dpwl0974 <dpwl0974@gmail.com>
2025-09-02 12:48:28 +0900

    feat: getFieldErrors() 와 스트림을 이용해서 모든 에러 메세지 보이기

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.stream.Collectors;
+
+
 @Controller
 public class PostController {
 
@@ -68,20 +71,19 @@ public class PostController {
     @PostMapping("/posts/doWrite")
     @ResponseBody
     public String doWrite(
-            @Valid PostWriteForm form,
-            BindingResult bindingResult
+            @Valid PostWriteForm form, BindingResult bindingResult
     ) {
 
         if(bindingResult.hasErrors()) {
-            FieldError fieldError = bindingResult.getFieldError();
-            String fieldName = fieldError.getField();
-            String errorMessage = fieldError.getDefaultMessage();
 
-            System.out.println("fieldName : " + fieldName);
-            System.out.println("errorMessage : " + errorMessage);
+            String fieldName = "title";
 
-            return getWriteFormHtml(errorMessage, form.title, form.content, fieldName);
+            String errorMessages = bindingResult.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining("<br>"));
 
+            return getWriteFormHtml(errorMessages, form.title, form.content, fieldName);
         }
         Post post = postService.write(form.title, form.content);
 
