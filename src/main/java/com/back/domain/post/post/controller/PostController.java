1e9c41cb1de9eeb5d7349ad67c24ee87f6ce87c0
dpwl0974 <dpwl0974@gmail.com>
2025-09-03 14:36:27 +0900

    feat: 글 목록 json 구현

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -10,10 +10,9 @@ import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class PostController {
@@ -64,6 +63,15 @@ public class PostController {
         model.addAttribute("post", post);
 
         return "post/detail";
+    }
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public List<Post> list() {
+
+        List<Post> posts = postService.findAll();
+
+        return posts;
     }
 
 }
